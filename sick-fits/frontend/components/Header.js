fd3a9622fce0545e0206de3093537632a70124c1
Troy Porter <troy.porter1@gmail.com>
2019-07-21 21:47:40 +0100

    Visualizing Route Changes

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,19 @@
-import Nav from './Nav'
 import Link from 'next/link'
+import Nav from './Nav'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import styled from 'styled-components'
+
+// https://nextjs.org/docs#router-events
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done()
+}
+Router.onRouteChangeError = () => {
+  NProgress.done()
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
