133d3bf1d00a6722a156c9c6b433f06b78b26a50
Troy Porter <troy.porter1@gmail.com>
2019-07-22 22:30:39 +0100

    Getting GraphQL Yoga Server Running

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' })
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+// TODO user express middleware to handle cookies (JWT)
+// TODO Use express middleware to populate current users
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  server => {
+    console.log(`Server is now running on port http://localhost:${server.port}`)
+  }
+)
