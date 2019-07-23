ab4f70ba80227fc43efa4e76848153da57445a4b
Troy Porter <troy.porter1@gmail.com>
2019-07-23 22:44:16 +0100

    Items Creation and Yoga Flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const Mutations = {}
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check for login
+
+    // passes db object in through context
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+
+      // also pass `info` because the query is inside here
+      // needs to pass the query to the backend
+      info
+    )
+
+    return item
+  }
+}
 
 module.exports = Mutations
