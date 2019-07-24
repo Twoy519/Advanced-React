886871284138d0de971492f768b9f0fee4bc2619
Troy Porter <troy.porter1@gmail.com>
2019-07-24 21:25:25 +0100

    Setting up Apollo Client with React

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,19 +1,41 @@
 import App, { Container } from 'next/app'
 import Page from '../components/Page'
+import { ApolloProvider } from 'react-apollo'
+import withData from '../lib/withData'
 
 class MyApp extends App {
+  // getInitialProps is a special NextJS lifecycle method
+  // This whole thing is needed because this is a server-rendered app
+  // getInitialProps runs before render()
+  // The return value becomes available inside render()
+  // What this does is crawl the page for Queries and Mutations
+  // and fires them off before we render the pag-e.
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query
+    return { pageProps }
+  }
+
   render() {
-    const { Component } = this.props
+    // `apollo` is accesible because we wrap MyApp
+    // with the `withData` higher-order component
+    const { Component, apollo, pageProps } = this.props
 
     return (
       <Container>
-        <Page>
-          {/* Component is going to be Sell or index */}
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            {/* Component is going to be Sell or index */}
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-export default MyApp
+export default withData(MyApp)
