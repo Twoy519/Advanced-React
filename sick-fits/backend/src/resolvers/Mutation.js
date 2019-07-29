6f9db5d1f8987507b12c8f166e965eed855cec70
Troy Porter <troy.porter1@gmail.com>
2019-07-29 22:22:30 +0100

    Updating Items with Queries and Mutations

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -16,6 +16,31 @@ const Mutations = {
     )
 
     return item
+  },
+
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args }
+    // remove the id from the udpates
+    // because we don't want to update the id
+    delete updates.id
+    // run the update method
+    // this is calling backend/src/generated/prisma.graphql::updateItem()
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      // need to include this becuase that is how
+      // the updateItem function knows what to return
+      // this part I don't get but Wes says:
+      // "the updateItem is expecting us to return an item
+      // so info will contain the query that we send it from
+      // the client side to return that item
+      info
+    )
   }
 }
 
